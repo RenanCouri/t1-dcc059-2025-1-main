489c2e14b4b5e9ebd1c9d42fa0c24e0e1ca52e69
Davi Rocha <davilrocha18@gmail.com>
2025-07-07 17:05:58 -0300

    Adiciona impressao em arquivos txt

--- src/Gerenciador.cpp
+++ src/Gerenciador.cpp
@@ -101,6 +101,69 @@ void Gerenciador::imprimirGrafo(Grafo* grafo){
 
     cout<<endl;
 }
+void Gerenciador::imprimeArquivo_a_ate_d(vector<char> resultado, string nome){
+    ofstream arq1(nome); //ofstream: apenas para saida de dados. abre o arquivo. cria o arquivo caso ele não exista.
+    size_t i = 1;
+    for (char id :resultado){
+        if(i == resultado.size()){
+            arq1 << id;
+        }
+        else{
+        arq1 << id << ",";
+        }
+        i++;
+    }
+
+    arq1.close();
+}
+
+void Gerenciador::imprimeArquivo_e_ate_g(Grafo* grafo, string nome){
+    ofstream arq2(nome);
+    for(No *no : grafo->lista_adj){
+        arq2 << no->id << ": ";
+        size_t j = 1;
+        for(Aresta *aresta : no->arestas){
+            if(j == no->arestas.size()){
+                arq2 << aresta->id_no_alvo;
+            }
+            else{
+                arq2 << aresta->id_no_alvo << " -> ";
+            }
+            j++;
+        }
+        arq2<<endl;
+    }
+    arq2.close();
+}
+
+void Gerenciador::imprimeArquivo_h(int raio, int diametro, vector<char> centro, vector<char> periferia, string nome){
+    ofstream arq1(nome); //ofstream: apenas para saida de dados. abre o arquivo. cria o arquivo caso ele não exista.
+    arq1 << raio << endl;
+    arq1 << diametro << endl;
+    size_t i = 1;
+    for (char id :centro){
+        if(i == centro.size()){
+            arq1 << id;
+        }
+        else{
+        arq1 << id << ",";
+        }
+        i++;
+    }
+    i = 1;
+    arq1 << endl;
+        for (char id :periferia){
+        if(i == periferia.size()){
+            arq1 << id;
+        }
+        else{
+        arq1 << id << ",";
+        }
+        i++;
+    }
+
+    arq1.close();
+}
 
 void Gerenciador::comandos(Grafo* grafo) {
 
@@ -134,7 +197,7 @@ void Gerenciador::comandos(Grafo* grafo) {
                     cout<<endl<<endl;
                     imprimirGrafoPorIndices(grafo,fecho_transitivo_direto);
                     if(pergunta_imprimir_arquivo("fecho_trans_dir.txt")) {
-                        cout<<"Metodo de impressao em arquivo nao implementado"<<endl<<endl;
+                        imprimeArquivo_a_ate_d(fecho_transitivo_direto,"fecho_trans_dir.txt");
                     }
 
 
@@ -150,7 +213,7 @@ void Gerenciador::comandos(Grafo* grafo) {
                     cout<<endl<<endl;
                     imprimirGrafoPorIndices(grafo,fecho_transitivo_indireto);
                     if(pergunta_imprimir_arquivo("fecho_trans_indir.txt")) {
-                        cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+                        imprimeArquivo_a_ate_d(fecho_transitivo_indireto,"fecho_trans_indir.txt");
                     }
 
                     ;
@@ -172,8 +235,8 @@ void Gerenciador::comandos(Grafo* grafo) {
                         cout << endl << endl;
                     }
 
-                    if (pergunta_imprimir_arquivo("caminho_minimo.txt")) {
-                        cout << "Metodo de impressao em arquivo nao implementado" << endl;
+                    if (pergunta_imprimir_arquivo("caminho_minimo_dijkstra.txt")) {
+                        imprimeArquivo_a_ate_d(caminho_minimo,"caminho_minimo_dijkstra.txt");
                     }
 
                     break;
@@ -187,7 +250,7 @@ void Gerenciador::comandos(Grafo* grafo) {
                     cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
 
                     if(pergunta_imprimir_arquivo("caminho_minimo_floyd.txt")) {
-                        cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+                        imprimeArquivo_a_ate_d(caminho_minimo_floyd,"caminho_minimo_floyd.txt");
                     }
 
 
@@ -206,7 +269,7 @@ void Gerenciador::comandos(Grafo* grafo) {
                         cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
 
                         if(pergunta_imprimir_arquivo("agm_prim.txt")) {
-                            cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+                           imprimeArquivo_e_ate_g(arvore_geradora_minima_prim,"agm_prim.txt");
                         }
 
                         delete arvore_geradora_minima_prim;
@@ -231,7 +294,7 @@ void Gerenciador::comandos(Grafo* grafo) {
                         imprimirGrafo(arvore_geradora_minima_kruskal);
 
                         if(pergunta_imprimir_arquivo("agm_kruskal.txt")) {
-                            cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+                            imprimeArquivo_e_ate_g(arvore_geradora_minima_kruskal,"agm_kruskal.txt");
                         }
 
                         delete arvore_geradora_minima_kruskal;
@@ -251,7 +314,7 @@ void Gerenciador::comandos(Grafo* grafo) {
                     imprimirGrafo(arvore_caminhamento_profundidade);
                     cout<<endl;
                     if(pergunta_imprimir_arquivo("arvore_caminhamento_profundidade.txt")) {
-                        cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
+                        imprimeArquivo_e_ate_g(arvore_caminhamento_profundidade,"arvore_caminhamento_profundidade.txt");
                     }
 
                     delete arvore_caminhamento_profundidade;
@@ -262,9 +325,8 @@ void Gerenciador::comandos(Grafo* grafo) {
                     vector<char> articulacao = grafo->vertices_de_articulacao();
                     cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
 
-                    if(pergunta_imprimir_arquivo("arvore_caminhamento_profundidade.txt")) {
-                        cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
-                    }
+                    if(pergunta_imprimir_arquivo("raio_diam_centro_perif.txt")) {
+                        imprimeArquivo_h(raio, diametro, centro, periferia,"raio_diam_centro_perif.txt");                    }
 
                     break;
                 }
--- src/Gerenciador.h
+++ src/Gerenciador.h
@@ -13,6 +13,10 @@ public:
     static vector<char> get_conjunto_ids(Grafo* grafo, int tam);
     static bool pergunta_imprimir_arquivo(string nome_arquivo);
     static void imprimirGrafo(Grafo* grafo);
+    static void imprimeArquivo_a_ate_d(vector<char> resultado, string nome);
+    static void imprimeArquivo_e_ate_g(Grafo* grafo, string nome);
+    static void imprimeArquivo_h(int raio, int diametro, vector<char> centro, vector<char> periferia, string nome);
+
 private:
     // Criei esta para imprimir a lista de fechos. Ainda falta colocar uma para imprimir no formato de grafo, seja baseando-se num grafo todo ou num grafo e numa lista dessas:
     static void imprimirConjuntoChar(vector<char> conjunto);  
